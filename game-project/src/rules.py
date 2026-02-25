aa431f9113e2677c25c01f7c28eba56b89530263
sonita-dzilna <sonita-anita.dzilna@edu.rtu.lv>
2026-02-25 21:16:52 +0000

    25.02.2026 23:016

--- game-project/src/rules.py
+++ game-project/src/rules.py
@@ -1,8 +1,5 @@
-from generator import num_sequence  # iegūt saģenerētu skaitļu virkni
-
 # Spēlētāju sākotnējie punkti
 p1, p2 = 100, 100
-current_player = 1  # spēle sākas ar spēlētāju 1
 
 # Noteikumu funkcija
 def apply_rules(p1, p2, current_player, number):
@@ -18,22 +15,15 @@ def apply_rules(p1, p2, current_player, number):
             p1 += 3
     elif number == 2:
         if current_player == 1:
-            p2 -= 4
-        else:
             p1 -= 4
+        else:
+            p2 -= 4
     elif number == 4:
         if current_player == 1:
-            p2 -= 8
-        else:
             p1 -= 8
+        else:
+            p2 -= 8
     return p1, p2
-
-# Pāreja cauri skaitļu virknei
-for number in num_sequence:  # jālieto tieši saraksts
-    p1, p2 = apply_rules(p1, p2, current_player, number)
-
-    # Pārslēgt spēlētāju nākamajam gājienam
-    current_player = 2 if current_player == 1 else 1
 
 print("Pēdējie rezultāti:")
 print("Spēlētājs 1:", p1)
